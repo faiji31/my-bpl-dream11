44b3a0dfb407dc4b7ad93a4a32a5f34dbb5d8a62
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-26 16:11:48 +0600

    4th commkit

---
+++ src/components/AvilablePlayers/AvilablePlayers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AvilablePlayers = () => {
+    return (
+        <div>
+            <h1>Avilable Players</h1>
+        </div>
+    );
+};
+
+export default AvilablePlayers;
