aff41a9fecab91ec8f0c6d1c87109d6de43c0e91
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-30 12:13:21 +0600

    8th commit

--- src/components2/AvilablePlayers/AvilablePlayers.jsx
+++ src/components2/AvilablePlayers/AvilablePlayers.jsx
@@ -6,42 +6,44 @@ const AvilablePlayers = ({PlayersPromise}) => {
     const PlayerData = use(PlayersPromise)
     console.log(PlayerData)
     return (
-        <div className='max-w-[1200px] mx-auto'>
+        <div className='max-w-[1200px] mx-auto gridgrid-cols-1 md:grid grid-cols-3 gap-4'>
             
-            <div className="card bg-base-100 w-96 shadow-sm p-4">
+       {
+        PlayerData.map(player=>     <div className="card bg-base-100  shadow-sm p-4">
   <figure>
-   <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+   <img className='w-full h-[400px] object-cover'
+      src={player.image}
       alt="Shoes" />
   </figure>
   <div className="mt-4">
     <div className='flex'>
-        <img src={userImg} alt="" /> <h2 className="card-title ml-2">Rohit Sharma</h2>
+        <img src={userImg} alt="" /> <h2 className="card-title ml-2">{player.name}</h2>
     </div>
   <div className='flex justify-between items-center border-b-1 border-gray-300 pb-2'>
       <div className='flex gap-4 mt-6 items-center'>
       <img className='w-[20px] h-[20px]'  src={FlagImg} alt="" /> 
-      <span>India</span>
+      <span>{player.country}</span>
     </div>
-    <button className='btn'>All-Rounder</button>
+    <button className='btn'>{player.role}</button>
   </div>
   <div className='flex justify-between font-bold'>
     <span>Rating</span>
-    <span>5</span>
+    <span>{player.rating}</span>
   </div>
   <div className='flex justify-between mt-4 '>
-    <span className='font-bold'>Right-Hand-Batsman</span>
-    <span>Right-Arm-Bowler</span>
+    <span className='font-bold'>{player.batting_style}</span>
+    <span>{player.bowling_style}</span>
   </div>
   <div className='flex justify-between items-center mt-3 '>
-    <p>Price: $1500000</p>
+    <p className='font-bold'>{player.price}</p>
     <button className='btn'>Choose</button>
   </div>
   
   
     
   </div>
-</div>
+</div>)
+       }
         </div>
     );
 };
