d0032b2c067b52f89457c0143a0ff8d2d7535ad7
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-31 14:09:28 +0600

    11th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 
 import './App.css'
 import { Suspense, useState } from 'react'
-import navImg from './assets/logo.png'
-import dollarImg from './assets/dollar.png'
 
 
 
+import NavBar from './NavBar/NavBar'
 import AvilablePlayers from './components2/AvilablePlayers/AvilablePlayers'
 import SelectedPlayers from './components2/SelectedPlayers/SelectedPlayers'
 
@@ -25,17 +24,8 @@ function App() {
 
   return (
     <>
-   
-      <div className="navbar max-w-[1200px] mx-auto">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl"><img className='w-[60px] h-[60px]' src={navImg}alt="logo" /></a>
-  </div>
-  <div className="flex items-center">
-    <span className='mr-1'>600000000</span>
-    <span className='mr-1'>Coin</span>
-    <img  src={dollarImg} alt="" className='w-6 h-6' />
-  </div>
-</div>
+   <NavBar></NavBar>
+
 <div className='flex justify-between items-center max-w-[1200px] mx-auto'>
   <h1 className='font-bold text-2xl'>Avilable Players</h1>
   <div>
