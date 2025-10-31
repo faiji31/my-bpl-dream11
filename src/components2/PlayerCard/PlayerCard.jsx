65789984580b46515dd5c8832f7f0151ef8b0f4c
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-31 14:16:24 +0600

    12th commit

---
+++ src/components2/PlayerCard/PlayerCard.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import userImg from "../../assets/user1.png"
+import FlagImg from '../../assets/Group.png'
+
+const PlayerCard = ({player}) => {
+    return (
+         <div className="card bg-base-100  shadow-sm p-4">
+  <figure>
+   <img className='w-full h-[400px] object-cover'
+      src={player.image}
+      alt="Shoes" />
+  </figure>
+  <div className="mt-4">
+    <div className='flex'>
+        <img src={userImg} alt="" /> <h2 className="card-title ml-2">{player.name}</h2>
+    </div>
+  <div className='flex justify-between items-center border-b-1 border-gray-300 pb-2'>
+      <div className='flex gap-4 mt-6 items-center'>
+      <img className='w-[20px] h-[20px]'  src={FlagImg} alt="" /> 
+      <span>{player.country}</span>
+    </div>
+    <button className='btn'>{player.role}</button>
+  </div>
+  <div className='flex justify-between font-bold'>
+    <span>Rating</span>
+    <span>{player.rating}</span>
+  </div>
+  <div className='flex justify-between mt-4 '>
+    <span className='font-bold'>{player.batting_style}</span>
+    <span>{player.bowling_style}</span>
+  </div>
+  <div className='flex justify-between items-center mt-3 '>
+    <p className='font-bold'>{player.price_usd_estimated}</p>
+    <button className='btn'>Choose</button>
+  </div>
+  
+  
+    
+  </div>
+</div>
+    );
+};
+
+export default PlayerCard;
